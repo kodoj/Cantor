a93a935da1763dfd4af2b5307c587d62cb92ae6a
Krzysztof Odoj <kodoj1@o2.pl>
2019-05-31 09:26:59 +0200

    clean code

--- src/main/java/com/futureprocessing/cantor/model/UserRegistrationDto.java
+++ src/main/java/com/futureprocessing/cantor/model/UserRegistrationDto.java
@@ -53,43 +53,111 @@ public class UserRegistrationDto {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getSurname() {
         return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getConfirmEmail() {
+        return confirmEmail;
+    }
+
+    public void setConfirmEmail(String confirmEmail) {
+        this.confirmEmail = confirmEmail;
+    }
+
+    public Boolean getTerms() {
+        return terms;
+    }
+
+    public void setTerms(Boolean terms) {
+        this.terms = terms;
     }
 
     public double getUsd() {
         return usd;
     }
 
+    public void setUsd(double usd) {
+        this.usd = usd;
+    }
+
     public double getEur() {
         return eur;
+    }
+
+    public void setEur(double eur) {
+        this.eur = eur;
     }
 
     public double getChf() {
         return chf;
     }
 
+    public void setChf(double chf) {
+        this.chf = chf;
+    }
+
     public double getRub() {
         return rub;
+    }
+
+    public void setRub(double rub) {
+        this.rub = rub;
     }
 
     public double getCzk() {
         return czk;
     }
 
+    public void setCzk(double czk) {
+        this.czk = czk;
+    }
+
     public double getGbp() {
         return gbp;
     }
 
+    public void setGbp(double gbp) {
+        this.gbp = gbp;
+    }
+
     public double getPln() {
         return pln;
+    }
+
+    public void setPln(double pln) {
+        this.pln = pln;
     }
 }
